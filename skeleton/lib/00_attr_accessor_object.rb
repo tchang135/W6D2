6045ee8692c4c4bfd467bc027ec976ca74f7f3e0
Timothy Chang <tchang135@gmail.com>
2022-12-13 17:27:49 -0500

    finished work for day

--- skeleton/lib/00_attr_accessor_object.rb
+++ skeleton/lib/00_attr_accessor_object.rb
@@ -1,6 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+      names.each {|name|
+        define_method(name) {
+          instance_variable_get("@#{name}")
+        }
+        define_method("#{name}=") { |val|
+          instance_variable_set("@#{name}", val)
+        }
       
+      }
   end
 end
